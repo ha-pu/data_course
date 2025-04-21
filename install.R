8bf865e8c70aad9f6f5e683db6671727853417ec
ha-pu <66355167+ha-pu@users.noreply.github.com>
2025-04-21 10:07:28 +0200

    Switch dependency management

---
+++ install.R
@@ -0,0 +1,13 @@
+install.packages(c(
+  "tidyverse",
+  "caret",
+  "caretensemble",
+  "ellmer",
+  "gbm",
+  "glmnet",
+  "randomForest",
+  "sjPlot",
+  "xgboost"# ,
+  #"IRkernel"
+))
+# IRkernel::installspec(user = FALSE)
